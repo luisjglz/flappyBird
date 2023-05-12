763ef368ff9ac295d04033023e25088dc8fe1ef9
Luis José González <ljgonzal@tec.mx>
2023-05-11 19:13:56 -0600

    some changes

--- flappy.py
+++ flappy.py
@@ -2,9 +2,9 @@
 
 Exercises
 
-1. Keep score.
-2. Vary the speed.
-3. Vary the size of the balls.
+1. Keep score. done
+2. Vary the speed. done
+3. Vary the size of the balls. done
 4. Allow the bird to move forward and back.
 """
 
@@ -15,7 +15,7 @@ from freegames import vector
 
 bird = vector(0, 0)
 balls = []
-
+puntos = 0
 
 def tap(x, y):
     """Move bird up in response to screen tap."""
@@ -24,8 +24,11 @@ def tap(x, y):
 
 
 def inside(point):
-    """Return True if point on screen."""
-    return -200 < point.x < 200 and -200 < point.y < 200
+    if (type(point) is dict):
+        return -200 < point["pos"].x < 200 and -200 < point["pos"].y < 200
+    else:    
+        """Return True if point on screen."""
+        return -200 < point.x < 200 and -200 < point.y < 200
 
 
 def draw(alive):
@@ -40,8 +43,19 @@ def draw(alive):
         dot(10, 'red')
 
     for ball in balls:
-        goto(ball.x, ball.y)
-        dot(20, 'black')
+        goto(ball["pos"].x, ball["pos"].y)
+        dot(ball["tam"], 'black')
+
+    """
+    Mostrar el puntaje en la pantalla
+    Primero muevo el puntero (turtle)
+    hacia el lugar donde quiero mostrar
+    el marcador y luego lo escribo en pantalla.
+    """
+    goto(160,170)
+    write(puntos, move=False, align='left', font=('Arial', 18, 'normal')) 
+
+
 
     update()
 
@@ -51,22 +65,30 @@ def move():
     bird.y -= 5
 
     for ball in balls:
-        ball.x -= 3
+        ball["pos"].x -= ball["vel"]
 
-    if randrange(10) == 0:
+    if randrange(20) == 0:
         y = randrange(-199, 199)
-        ball = vector(199, y)
+        ball = {"pos":vector(199, y),"vel":randrange(3,8),"tam":randrange(15,25)}
         balls.append(ball)
 
     while len(balls) > 0 and not inside(balls[0]):
         balls.pop(0)
+        """
+        Hice punto (salió el ball por la izq)
+        Especificar "global" para que modifique
+        la variable puntos definida al inicio.
+        """
+        global puntos
+        puntos += 1
+
 
     if not inside(bird):
         draw(False)
         return
 
     for ball in balls:
-        if abs(ball - bird) < 15:
+        if abs(ball["pos"] - bird) < 15:
             draw(False)
             return
 
